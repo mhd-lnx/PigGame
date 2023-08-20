8a2773bf21296655968cace9cc4979fa9682ec0f
Mahdi <mxhdi.sh@gmail.com>
2023-08-20 23:50:18 +0330

    new button functionality and reset everything

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const current1El = document.querySelector("#current--1");
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
-const scores =[0,0];
+let scores =[0,0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true ;
@@ -77,4 +77,16 @@ btnHold.addEventListener("click",function(){
         
         
 })
-
+btnNew.addEventListener("click",function(){
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    document.querySelector(`.player--${activePlayer}`).classList.remove("player--winner");
+    document.querySelector(".player--0").classList.add("player--active");
+    document.querySelector(".player--1").classList.remove("player--active");
+    scores =[0,0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true ;
+})
