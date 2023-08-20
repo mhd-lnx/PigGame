3e64fc8a3738cbaad9598e3cb329f716b3d76e09
Mahdi <mxhdi.sh@gmail.com>
2023-08-20 17:25:07 +0330

    swtich active player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting elements 
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
 const diceEl = document.querySelector(".dice");
@@ -34,6 +36,7 @@ btnRoll.addEventListener("click",function(){
         document.querySelector(`#current--${activePlayer}`).textContent = 0;
         activePlayer = activePlayer ===0 ? 1:0;
         currentScore = 0
-
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
     }
 })
