efabf13e7d545d76ec547e854ffae154ca1fece4
Mahdi <mxhdi.sh@gmail.com>
2023-08-20 13:40:03 +0330

    initialize 3 buttons and working on rolling dice functionality

--- script.js
+++ script.js
@@ -4,8 +4,20 @@
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
 const diceEl = document.querySelector(".dice");
-
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+// Rolling dice functionality
+btnRoll.addEventListener("click",function(){
+    // 1 generating a random dice roll
+    let dice = Math.trunc(Math.random()*6)+1;
+
+    // 2 display dice
+    diceEl.classList.remove("hidden");
+    // 3 check for rolled 1 and if true , switch to next player
+})
